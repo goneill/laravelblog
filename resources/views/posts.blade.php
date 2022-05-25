d7d84697fd6a94ec18a42adfa89f517a4c6ba531
goneill <grainne@oandh.net>
2022-05-25 19:38:36 -0400

    updating with what was on laptop

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+
+
+    @foreach ($posts as $post) 
+         <article>
+            <a href="/posts/{{$post->slug}}">
+                <h1>{{ $post->title}}</h1>
+            </a>
+        
+            <div>
+
+                {{$post->excerpt}}
+            </div>
+        </article>
+
+    @endforeach
+
+
+</x-layout>
